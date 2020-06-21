a0591656b4859aa857c4030d915eab1fa68c4545
VilmantasN <vilmantas.neviera@gmail.com>
2020-06-21 19:43:27 +0300

    titulinis

--- src/Components/base/landingImage.jsx
+++ src/Components/base/landingImage.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import Styled from 'styled-components'
+import { Paragraph } from './paragraph';
 
 
 const LandingImageStyle = Styled.img`
-    width: 50px;
+    position: relative;
+    width: 85vw;        
+    height: 60vh;
 `;
 
-export const LandingImage = () => {
+const LandingPageTextSquare = Styled.div`
+    position: relative;
+    text-align: left;
+    width: 35%;
+    min-height: 250px;
+    margin: 0 auto;
+    bottom: 90px;
+    background-color: #F2F2F2;
+    font-size: 50px;
+    padding: 50px 100px;
+
+    -moz-box-shadow:    3px 3px 5px 6px #ccc;
+    -webkit-box-shadow: 3px 3px 5px 6px #ccc;
+    box-shadow:         1px 1px 1px 1px #ccc;
+    border-radius: 2px;
+
+    
+
+`;
+export const LandingImage = (props) => {
     return (
         <div >
-            <LandingImageStyle src='https://wallpaperaccess.com/full/124587.jpg'/>
+            <LandingImageStyle src={props.url}/>
+            <LandingPageTextSquare>
+                <Paragraph>{props.Text}</Paragraph>
+            </LandingPageTextSquare>
         </div>
     )
 }
--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import {LandingImage} from '../../Components/base/landingImage'
+import {ContentContainer} from '../../Components/base/container'
+import {LandingPageText} from '../../Components/base/landingPageText'
+
+
+
 
 export const Main = () => {
   return <div>
-    <LandingImage></LandingImage>
+    <LandingImage Text={"Kodėl rinktis mus? SIRPK"} url={'https://images.unsplash.com/photo-1505075625100-1a9150eacf21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'} />
+    <div className="square">
+
+    </div>
+    <ContentContainer>
+      <LandingPageText>
+      </LandingPageText>
+    </ContentContainer>
   </div>
 }
